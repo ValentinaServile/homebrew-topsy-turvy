ca6edbdb71de55312bc7cdb4fbcdd85f77326683
Valentina Servile <valentina.servile@gmail.com>
2020-08-20 17:15:42 +0200

    Upgrade sha256

--- Formula/topsy-turvy.rb
+++ Formula/topsy-turvy.rb
@@ -2,7 +2,7 @@ class TopsyTurvy < Formula
   desc "Upside down text"
   homepage "https://github.com/ValentinaServile/topsy-turvy"
   url "https://github.com/ValentinaServile/topsy-turvy/releases/download/v0.2.0/topsy-turvy-0.2.0.tar.gz"
-  sha256 "2fa0e91e486a3c44b5cc4fd6a0eeedc8596fcae8749766cba5989775c095da77"
+  sha256 "3f1fdaa9269988f13a513e4fc89dc0077912f22c8e4b5828f7f93e9e2b2ec869"
   license "https://github.com/ValentinaServile/topsy-turvy/blob/master/LICENSE.md"
   version "0.2.0"
 
