004a76f2a6439c8304c3da2beba66d7f809d2ed8
Valentina Servile <valentina.servile@gmail.com>
2020-08-20 17:14:20 +0200

    Upgrade to version 0.2.0

--- Formula/topsy-turvy.rb
+++ Formula/topsy-turvy.rb
@@ -1,10 +1,10 @@
 class TopsyTurvy < Formula
   desc "Upside down text"
   homepage "https://github.com/ValentinaServile/topsy-turvy"
-  url "https://github.com/ValentinaServile/topsy-turvy/releases/download/v0.1.0/topsy-turvy-0.1.0.tar.gz"
+  url "https://github.com/ValentinaServile/topsy-turvy/releases/download/v0.2.0/topsy-turvy-0.2.0.tar.gz"
   sha256 "2fa0e91e486a3c44b5cc4fd6a0eeedc8596fcae8749766cba5989775c095da77"
   license "https://github.com/ValentinaServile/topsy-turvy/blob/master/LICENSE.md"
-  version "0.1.0"
+  version "0.2.0"
 
   def install
     bin.install "topsy-turvy"
